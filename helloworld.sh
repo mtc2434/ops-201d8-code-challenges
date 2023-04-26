c44d8319e05056d86889b22f6c807f0c053864fd
Marcelo <130250660+mtc2434@users.noreply.github.com>
2023-04-25 19:08:13 -0700

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,3 +1,20 @@
+
+#!/bin/bash
+
+# Script Name:                  helloworld.sh
+# Author:                       Marcelo Clark
+# Date of latest revision:      04/25/2023
+# Purpose:                      Practice initial coding with shell script to print a string
+
+# Declaration of variables       str="I am working perfectly fine!": creates a variable that puts text inside
+
+# Declaration of functions        echo: prints the text you add 
+
+# Main
 echo "Hello World"
 str="I am working perfectly!"
+
 echo $str
+
+
+# End
