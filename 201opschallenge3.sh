dd9d99f8b7d1d422c02acfa4e07bddffaa03cb77
marcelo <m01t02c03@gmail.com>
2023-04-28 03:01:03 +0000

    test

---
+++ 201opschallenge3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script:  201opschallenge3.sh                     
+# Author:  Marcelo Clark                      
+# Date of latest revision:   4/27/2023   
+# Purpose:   create an array                   
+
+# Declare global variables
+folders=("dir1" "dir2" "dir3" "dir4")
+
+
+
+# Declare functions
+
+# Main
+echo ${folders[1]} 
+echo "Hello World"
