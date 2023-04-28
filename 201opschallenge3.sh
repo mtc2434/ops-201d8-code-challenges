7878f9f3400ce21b56f915f2ff244534f6ae87a1
marcelo <m01t02c03@gmail.com>
2023-04-28 22:06:40 +0000

    201opschallenge3

--- 201opschallenge3.sh
+++ 201opschallenge3.sh
@@ -12,6 +12,11 @@ folders=("dir1" "dir2" "dir3" "dir4")
 
 # Declare functions
 
-# Main
-echo ${folders[1]} 
-echo "test"
+# Main 
+# Had trouble pushing. had to go to settings and click off on git terminal authenticate, turn on again after window closes)
+
+#The indices are called within the array, and the touch command creates the text file within the corresponding directory. The forward slash makes the pathing go from the directory itself to inside the directory
+touch "${folders[0]}/directory1.txt"
+touch "${folders[1]}/directory2.txt"
+touch "${folders[2]}/directory3.txt"
+touch "${folders[3]}/directory4.txt"
