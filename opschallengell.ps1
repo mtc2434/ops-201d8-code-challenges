afabbf03a0008cd69dd41fc032c4075bb350daaa
Marcelo <130250660+mtc2434@users.noreply.github.com>
2023-05-08 23:21:48 -0700

    Create opschallengell.ps1

---
+++ opschallengell.ps1
@@ -0,0 +1,34 @@
+# Script:  201opschallenge11.sh                     
+# Author:  Marcelo Clark                      
+# Date of latest revision:   5/08/2023
+
+#purpose Enable file and printer sharing, allowing ICMP traffic, Enable remote management, remove bloatware, enable hyper-v, disable smbv1 		    
+
+#sources: https://learn.microsoft.com/en-us/windows-server/storage/file-server/troubleshoot/detect-enable-and-disable-smbv1-v2-v3 https://www.makeuseof.com/tag/easily-remove-bloatware-windows-10/ 
+
+#Declaration of variables:
+
+
+#Declaration of functions:
+
+#disclaimer! run powershell in admin mode
+
+#Main
+
+# Enables file and printer sharing
+Set-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Enabled True
+
+# Creates new rule to allow ICMP traffic
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
+
+# Enables remote management
+Enable-PSremoting -Force
+
+#Remove bloatware
+Get-AppxPackage -name "microsoft.appname" | Remove-AppxPackage
+
+# Enables Hypervisors
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
+
+#disables smbv1 
+Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
