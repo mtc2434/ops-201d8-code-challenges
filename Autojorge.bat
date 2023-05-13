e45472538aafa032f9ff27866b13611ccea21c10
Marcelo <130250660+mtc2434@users.noreply.github.com>
2023-05-13 14:00:17 -0700

    Update Autojorge.bat
    updated challenge 8

--- Autojorge.bat
+++ Autojorge.bat
@@ -1,1 +1,12 @@
-start notepad.exe 
+# Script Name:               Autojorge.ps1
+# Author:                    Marcelo Clark
+# Date of latest revision:   5/13/2023
+# Purpose:                   To backup the files from Jorge to a separate location
+
+#Declaration of variables:
+
+
+#Declaration of functions:
+
+#Main
+robocopy C:\Users\J.thompson\Desktop\WorkFiles C:\Users\J.thompson\Documents /S
